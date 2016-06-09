fd131eca333a4d50de8edef7ab8f004a73aebf7c
ibajric <sssuperida@gmail.com>
2016-06-09 23:42:17 +0200

    nova activity popis biljaka, gumb na početnoj vodi na nju

---
+++ app/src/main/java/hr/unipu/hortus/PopisBiljakaActivity.java
@@ -0,0 +1,13 @@
+package hr.unipu.hortus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PopisBiljakaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popis_biljaka);
+    }
+}
