89a815d42727e885b33017da9bed00d7e3b37894
kprizm <kprizm@unipu.hr>
2016-06-11 15:35:51 +0200

    Testiranje prikaza biljaka u listi

--- app/src/main/java/hr/unipu/hortus/Biljka.java
+++ app/src/main/java/hr/unipu/hortus/Biljka.java
@@ -4,6 +4,63 @@ package hr.unipu.hortus;
 public class Biljka {
     String ime;
     String opis;
+    boolean otvoreno;
     int mjesec_sadnje;
     int mjesec_berbe;
+
+    public Biljka() {
+    }
+
+    public Biljka(String ime, String opis, boolean otvoreno, int mjesec_sadnje, int mjesec_berbe) {
+        this.ime = ime;
+        this.opis = opis;
+        this.otvoreno = otvoreno;
+        this.mjesec_sadnje = mjesec_sadnje;
+        this.mjesec_berbe = mjesec_berbe;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public int getMjesec_sadnje() {
+        return mjesec_sadnje;
+    }
+
+    public void setMjesec_sadnje(int mjesec_sadnje) {
+        this.mjesec_sadnje = mjesec_sadnje;
+    }
+
+    public int getMjesec_berbe() {
+        return mjesec_berbe;
+    }
+
+    public void setMjesec_berbe(int mjesec_berbe) {
+        this.mjesec_berbe = mjesec_berbe;
+    }
+
+    public boolean isOtvoreno() {
+        return otvoreno;
+    }
+
+    public void setOtvoreno(boolean otvoreno) {
+        this.otvoreno = otvoreno;
+    }
+
+    @Override
+    public String toString() {
+        return ime;
+    }
 }
--- app/src/main/java/hr/unipu/hortus/PopisBiljakaActivity.java
+++ app/src/main/java/hr/unipu/hortus/PopisBiljakaActivity.java
@@ -1,7 +1,12 @@
 package hr.unipu.hortus;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PopisBiljakaActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class PopisBiljakaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popis_biljaka);
+
+        /*izvor: http://www.cipro.hr/kalendar-radova-vrt.htm#.V1vTsRyKS70 */
+        List<Biljka> popis_biljaka = new ArrayList<>();
+        // naziv, opis, otvoreno?, mjesec-sadnje, mjesec-berbe
+        popis_biljaka.add(new Biljka("Paprika", "Bla bla", false, 2, 9));
+        popis_biljaka.add(new Biljka("Rajčica", "Bla bla", false, 2, 9));
+        popis_biljaka.add(new Biljka("Krastavac", "Bla bla", true, 4, 9));
+
+        ArrayAdapter<Biljka> biljkaArrayAdapter = new ArrayAdapter<Biljka>(this, android.R.layout.simple_list_item_1, popis_biljaka);
+        ListView listView = (ListView) findViewById(R.id.biljke_listView);
+        listView.setAdapter(biljkaArrayAdapter);
     }
 }
