6a13e758edf0876d4c50a65dbc48d7fe57a20a34
Nino <ndasko@unipu.hr>
2016-06-13 00:59:51 +0200

    Ime odabrane biljke prikazuje se kao naslov ekrana detalja

--- app/src/main/java/hr/unipu/hortus/BiljkaDetaljActivity.java
+++ app/src/main/java/hr/unipu/hortus/BiljkaDetaljActivity.java
@@ -2,6 +2,9 @@ package hr.unipu.hortus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class BiljkaDetaljActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class BiljkaDetaljActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_biljka_detalj);
+
+        List<Biljka> popis_biljaka = Podaci.dajPodatke();
+        int broj_biljke = getIntent().getIntExtra("broj_biljke", 0);
+        Biljka odabrana_biljka = popis_biljaka.get(broj_biljke);
+
+        TextView ime_tekst = (TextView) findViewById(R.id.ime_biljke);
+        ime_tekst.setText(odabrana_biljka.getIme());
     }
 }
