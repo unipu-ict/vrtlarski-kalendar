7dcad6f988e362170991174485d807911a5b1f09
Nino <ndasko@unipu.hr>
2016-06-13 00:18:02 +0200

    Novi ekran koji će prikazivati detalje o odabranoj biljci

---
+++ app/src/main/java/hr/unipu/hortus/BiljkaDetaljActivity.java
@@ -0,0 +1,13 @@
+package hr.unipu.hortus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BiljkaDetaljActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_biljka_detalj);
+    }
+}
